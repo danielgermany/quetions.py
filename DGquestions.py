0af8aa1568d775e95036628d1c40bd1f88f4ca2d
csPython <cspython@AD25TF04NGF1J-89.local>
2018-12-10 10:58:05 -0500

    Revising code. Removed the try and execpt funtions for now

--- DGquestions.py
+++ DGquestions.py
@@ -10,21 +10,23 @@ print("1. 3")
 print("2. 9")
 print("3. 81")
 print("4. 18")
-    
-while True:
+
+while True: #Setting up loop
+    #Asks the user to answer
     user_answer = input("Enter your answer without anything else.     ")
-    try:
-        if(user_answer == '1'):
-            print("Your answer is incorrect")
-            break
-        elif(user_answer == '2'):
-            print("Your answer is incorrect")
-            break
-        elif(user_answer == '3'):
-            print("Your answer is correct")
-            break
-        elif(user_answer == '4'):
-            print("Your answer is incorrect")
-            break
-    except:
+        #Checks if answer given is right or not
+    if(user_answer == '1'):
+        print("Your answer is incorrect")
+        break
+    elif(user_answer == '2'):
+        print("Your answer is incorrect")
+        break
+    elif(user_answer == '3'):
+        print("Your answer is correct")
+        break
+    elif(user_answer == '4'):
+        print("Your answer is incorrect")
+        break
+    else:
+        #If the user gives anything other than 1,2,3, or 4 than the program returns this.
         print("The answer you gave is either not one of the asnwer choices or no vaiable for use.")
