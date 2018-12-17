9024133a33dbd89c2005ecbe6e5c6569eb58503c
csPython <cspython@AD25TF04NGF1J-89.local>
2018-12-17 10:50:56 -0500

    Updating code

--- DGquestions.py
+++ DGquestions.py
@@ -1,6 +1,8 @@
 user_answer = str
+user_answerConfirm = str
 user_score = 0
 ans_check = True
+ans_checkConfirm = True
 
 #Asks the user a question
 print("What is 9 squared?")
@@ -15,20 +17,49 @@ print("4. 18")
 
 while ans_check: #Setting up loop
     #Asks the user to answer
-    user_answer = input("Enter your answer without anything else.     ")
+    print("Enter your answer without anything else.")
+    user_answer = input()
     #Checks if answer given is right or not
     try:
         user_answer = int(user_answer)
         if user_answer < 1:
-            print("This isn't a valid answer")
+            print("This isn't a valid answer, this number was either 0 or a negative number.")
+            print("Please only enter 1, 2, 3, or 4 as viable answers.")
             continue
         elif user_answer > 4:
-            print("This isn't a valid answer")
+            print("This isn't a valid answer, this number was higher than 4.")
+            print("Please only enter 1, 2, 3, or 4 as viable answers.")
             continue
-        elif user_answer == 3:
-            user_score +=1
+        else:
             ans_check = False
-            continue
     except ValueError:
-        print("Please only enter whole numbers")
+        print("You answer was a string.")
+        print("Please only enter 1, 2, 3, or 4 as viable answers.")
+
+
+while ans_checkConfirm: #Setting up loop for confirmation of asnwer
+    #Asks the user to answer confirmation question.
+    print("Confirm that your answer is ", user_answer, " by entering you answer again.")
+    user_answerConfirm = input()
+    try:
+        user_answerConfirm = int(user_answerConfirm)
+        if user_answerConfirm < 1:
+            print("This isn't a valid answer, this number was either 0 or a negative number.")
+            print("Please only enter 1, 2, 3, or 4 as viable answers.")
+            continue
+        elif user_answerConfirm> 4:
+            print("This isn't a valid answer, this number was higher than 4.")
+            print("Please only enter 1, 2, 3, or 4 as viable answers.")
+            continue
+        elif user_answerConfirm == user_answer:
+            ans_checkConfirm = False
+            print("Your answer has been confirmed, proceeding to the next question.")
+            continue
+        else:
+            print("Your original answer and confirmed answer dosn't match.")
+    except ValueError:
+        print("You answer was a string.")
+        print("Please only enter 1, 2, 3, or 4 as viable answers.")
+
+
 
