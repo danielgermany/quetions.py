57d324be193c1629cfe454900aaceddbf270e9a5
danielgermany <45012910+danielgermany@users.noreply.github.com>
2019-01-15 11:24:50 -0500

    Updating comments in code.

--- DGquizVar.py
+++ DGquizVar.py
@@ -1,3 +1,4 @@
+#Defining variables
 user_answerConfirm = str
 user_answer = str
 user_score = 0
@@ -7,13 +8,14 @@ end = True
 x = 0
 y = 0
 
+#Assigning questions and answers to each position in the list
 question1 = ("""Who was the Greek or Roman God of War?
 1. Ares
 2. Athena
 3. Zues
 4. Apollo""")
 question1_correct = 1
-question1_userAnswer = int
+question1_userAnswer = str
 
 Q1 = [question1,question1_userAnswer,question1_correct]
 
@@ -23,7 +25,7 @@ question2 = ("""What chemical element is diamond made of?
 3. Carbon
 4. Boron""")
 question2_correct = 3
-question2_userAnswer = int
+question2_userAnswer = str
 
 Q2 = [question2,question2_userAnswer,question2_correct]
 
@@ -33,7 +35,7 @@ question3 = ("""What is the name of the poker hand containing three of a kind an
 3. Three of a Kind
 4. Two Pair""")
 question3_correct = 2
-question3_userAnswer = int
+question3_userAnswer = str
 
 Q3 = [question3,question3_userAnswer,question3_correct]
 
@@ -43,7 +45,7 @@ question4 = ("""What part of the body produces insulin?
 3. Brain
 4. Liver""")
 question4_correct = 1
-question4_userAnswer = int
+question4_userAnswer = str
 
 Q4 = [question4,question4_userAnswer,question4_correct]
 
@@ -53,7 +55,7 @@ question5 = ("""In a standard pack of cards, which king is the only one to not h
 3. King of Hearts
 4. King of Diamonds""")
 question5_correct = 3
-question5_userAnswer = int
+question5_userAnswer = str
 
 Q5 = [question5,question5_userAnswer,question5_correct]
 
@@ -63,7 +65,7 @@ question6 = ("""How many syllables make up a haiku?
 3. 14
 4. 17""")
 question6_correct = 4
-question6_userAnswer = int
+question6_userAnswer = str
 
 Q6 = [question6,question6_userAnswer,question6_correct]
 
@@ -73,7 +75,7 @@ question7 = ("""What is the standard unit of distance in the metric system?
 3. Planck length
 4. Kilometers""")
 question7_correct = 1
-question7_userAnswer = int
+question7_userAnswer = str
 
 Q7 = [question7,question7_userAnswer,question7_correct]
 
@@ -83,7 +85,7 @@ question8 = ("""What is the official language of Brazil?
 3. Portuguese
 4. French""")
 question8_correct = 3
-question8_userAnswer = int 
+question8_userAnswer = str
 
 Q8 = [question8,question8_userAnswer,question8_correct]
 
@@ -93,7 +95,7 @@ question9 = ("""What superhero has been played by Michael Keaton, Val Kilmer, Ge
 3. Spider-Man
 4. Batman""")
 question9_correct = 4
-question9_userAnswer = int
+question9_userAnswer = str
 
 Q9 = [question9,question9_userAnswer,question9_correct]
 
@@ -103,13 +105,15 @@ question10 = ("""Does 2 + 2 = 5?
 3. 1984
 4. idk""")
 question10_correct = 3
-question10_userAnswer = int
+question10_userAnswer = str
 
 Q10 = [question10,question10_userAnswer,question10_correct]
 
 quiz = [Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10]
 
+#Defining the function being used to ask the questions
 def question_Confirm():
+    #Making the vars in function global
     global ans_check
     global ans_checkConfirm
     global user_answer
@@ -145,8 +149,7 @@ def question_Confirm():
                 user_answerConfirm = int(user_answerConfirm)
                 if user_answer == user_answerConfirm:
                     ans_checkConfirm = False
-                    print(quiz[x][1])
-                    print(quiz[x][2])
+                    quiz[x][1] = int(quiz[x][1])
                 else:
                     print("Your original answer and confirmed answer dosn't match.")
                     print("Returning you to the original question.")
@@ -157,11 +160,13 @@ def question_Confirm():
                 print("You answer was a string.")
                 print("Please only enter 1, 2, 3, or 4 as viable answers.")
                 ans_check = True
-
+        #This makes sure the quiz moves on to the next question
         if ans_check == False and ans_checkConfirm == False:
             ans_check = True
             ans_checkConfirm = True
             x += 1
+
+            #When reaching the end of the quiz end the while loop
             if x > 9:
                 end = False
             
@@ -172,6 +177,7 @@ def question_Grade():
     global user_score
     ans_check = True
     ans_checkConfirm = True
+    #This for loop grades the user's quiz
     for y in range(9):
         if quiz[y][1] == quiz[y][2]:
             user_score += 1
